3a86b3e5abc873c169cccf17314182ca20e4285b
JorenBuekers <joren.buekers@isglobal.com>
2025-05-26 17:06:57 +0200

    read .cwa data

--- pa_proc/acc_to_intermediate.py
+++ pa_proc/acc_to_intermediate.py
@@ -23,56 +23,103 @@ def acc_to_intermediate(path, device, sampling_freq, autocalibration):
     ################
 
     if device == 'expoapp-csv':
+
+        # Extract datetime information
         header = pd.read_csv(path, nrows=2, header=None)
         start_date = header.iloc[1,0][-10:].strip()
-        start_second = np.int32(header.iloc[0,0][-2:].strip())
+        start_second = np.int32(header.iloc[0,0][-2:].strip()) # (data before the first rounded second was already removed in expoapp_to_acc.py)
         start_minute = np.int32(header.iloc[0,0][-5:-3].strip())
         start_time = header.iloc[0,0][-8:-5].strip() + "{:02d}".format(start_minute) + ':00' 
         start_datetime = (datetime.datetime.strptime(start_date + ' ' + start_time, '%d/%m/%Y %H:%M:%S').
                         strftime('%Y-%m-%d %H:%M:%S'))
-        samples_to_skip_until_first_rounded_minute = 0 if (60-start_second)*30==1800 else (60-start_second)*30 # start at first rounded minute
+        
+        # Identify location of first rounded minute (data of the incomplete minute before will be deleted)
+        loc_first_round_minute = 0 if (60-start_second)*sampling_freq==1800 else (60-start_second)*sampling_freq # start at first rounded minute
 
+        # Load acceleration time series 
         timeseries = pd.read_csv(path, 
-                                 skiprows = 3 + samples_to_skip_until_first_rounded_minute,
-                                 names=['v', 'ml', 'ap']) #The axis order is vertical, horizontal and perpendicular
+                                 skiprows = 3 + loc_first_round_minute, # start at first rounded minute
+                                 names=['v', 'ml', 'ap']) #The axis order is vertical, mediolateral and anterioposterior
     
     elif device == 'actigraph-csv':
 
-        # Extract datetime information from the Actigraph-like file
+        # Extract datetime information
         header = pd.read_csv(path, nrows=10, header=None)
         start_date = header.iloc[3,0][-10:].strip()
-        start_second = np.int32(header.iloc[2,0][-2:].strip())
+        start_second = np.int32(header.iloc[2,0][-2:].strip()) # (actigraph csv files always start at first rounded second)
         start_minute = np.int32(header.iloc[2,0][-5:-3].strip())
         start_time = header.iloc[2,0][-8:-5].strip() + "{:02d}".format(start_minute) + ':00' 
         start_datetime = (datetime.datetime.strptime(start_date + ' ' + start_time, '%d/%m/%Y %H:%M:%S').
                         strftime('%Y-%m-%d %H:%M:%S'))
-        samples_to_skip_until_first_rounded_minute = 0 if (60-start_second)*30==1800 else (60-start_second)*30 # start at first rounded minute
+        
+        # Identify location of first rounded minute (data of the incomplete minute before will be deleted)
+        loc_first_round_minute = 0 if (60-start_second)*sampling_freq==1800 else (60-start_second)*sampling_freq 
 
+        # Load acceleration time series 
         timeseries = pd.read_csv(path,  sep = ' ',
-                             skiprows= 11 + samples_to_skip_until_first_rounded_minute, 
-                             names=['xyz']) # Specify the separator so everything will be loaded into 1 column. ADAPT TO STUDY (e.g. actilife uses dots as separators).
+                             skiprows= 11 + loc_first_round_minute, # start at first rounded minute
+                             names=['xyz']) # Specify the separator so everything will be loaded into 1 column. 
         timeseries = timeseries['xyz'].str.split(',', expand=True)
-        timeseries = timeseries.rename(columns={0: "ml", 1: "v", 2: "ap"}) #The axis order is horizontal, vertical and perpendicular
+        timeseries = timeseries.rename(columns={0: "ml", 1: "v", 2: "ap"}) # The axis order is mediolateral, vertical and anterioposterior
         timeseries = timeseries.astype(float)
         timeseries = timeseries[['v', 'ml', 'ap']] # Change order of axes
     
     elif device == 'actigraph-gt3x': 
+        
+        # Load acceleration time series 
         from pygt3x.reader import FileReader
         with FileReader(path) as reader:
             timeseries = reader.to_pandas()
         timeseries.index = pd.to_datetime(timeseries.index, unit='s')
-        start_datetime = str(timeseries.index[0])
-        start_second = np.int32(str(timeseries.index[0])[-2:])
-        samples_to_skip_until_first_rounded_minute = 0 if (60-start_second)*30==1800 else (60-start_second)*30 # start at first rounded minute
+
+        # Identify location of first rounded minute (data of the incomplete minute before will be deleted)
+        first_round_minute = timeseries.loc[timeseries.index.minute==timeseries.index[0].minute+1].index[0]
+        loc_first_round_minute = timeseries.index.get_loc(first_round_minute)
+        loc_first_round_minute = 0 if loc_first_round_minute==sampling_freq*60 else loc_first_round_minute
+
+        # Remove data before first rounded minute
+        timeseries = timeseries[loc_first_round_minute:]
+
+        # Extract datetime information
+        start_datetime = timeseries.index[0]
+        start_second = start_datetime.second
+
+        # Adapt index and column names
         timeseries.reset_index(inplace=True)
-        timeseries = timeseries.rename(columns={"X": "ml", "Y": "v", "Z": "ap"})
+        timeseries = timeseries.rename(columns={"X": "ml", "Y": "v", "Z": "ap"}) # The axis order is mediolateral, vertical and anterioposterior
         timeseries = timeseries[['v', 'ml', 'ap']] # Change order of axes and drop unnecesarry columns
+    
+    elif device == 'axivity-cwa': 
+
+        # Load acceleration time series 
+        from openmovement.load import CwaData
+        with CwaData(path, include_gyro=False, include_temperature=False) as cwa_data:
+            timeseries = cwa_data.get_samples()
+        timeseries.set_index("time", inplace=True)
+        
+        # Identify location of first rounded minute (data of the incomplete minute before will be deleted)
+        first_round_minute = timeseries.loc[timeseries.index.minute==timeseries.index[0].minute+1].index[0]
+        loc_first_round_minute = timeseries.index.get_loc(first_round_minute)
+        loc_first_round_minute = 0 if loc_first_round_minute==sampling_freq*60 else loc_first_round_minute
+
+        # Remove data before first rounded minute
+        timeseries = timeseries[loc_first_round_minute:]
+
+        # Extract datetime information
+        start_datetime = timeseries.index[0]
+        start_second = start_datetime.second
+        
+        # Adapt index and column names
+        timeseries.reset_index(inplace=True)
+        timeseries = timeseries.rename(columns={"accel_x": "v", "accel_y": "ml", "accel_z": "ap"}) # The axis order is vertical, mediolateral and anterioposterior
+        timeseries = timeseries[['v', 'ml', 'ap']] # Change order of axes and drop unnecesarry columns
+
     
     # Calculate counts for the different axes and combine
     counts = get_counts(np.array(timeseries), freq=sampling_freq, epoch=60)
-    counts = counts = pd.DataFrame(counts, columns=['counts_v', 'counts_ml', 'counts_ap'])  #The axis correspond to 1=vertical, 2=horizontal and 3=perpendicular
+    counts = pd.DataFrame(counts, columns=['counts_v', 'counts_ml', 'counts_ap'])  #The axis correspond to 1=vertical, 2=horizontal and 3=perpendicular
     counts['counts_vm'] = np.sqrt(np.sum(np.square(np.array(counts)), axis=1))
-    counts.index = np.datetime64(start_datetime) + np.arange(0,len(counts.index)*60,60)
+    counts.index = np.datetime64(start_datetime, "s") + np.arange(0,len(counts.index)*60,60)
 
     # Apply choi algorithm to vector magnitude of Actigraph counts
     counts['wear_choi'] = weartime_choi2011(np.array(counts[["counts_v","counts_ml","counts_ap"]]), 
@@ -99,7 +146,7 @@ def acc_to_intermediate(path, device, sampling_freq, autocalibration):
     # Downsample to 1 Hz and add info on wear based on van Hees 2011
     enmo_1s = pd.DataFrame({'enmo': enmo.groupby(enmo.index // sampling_freq).mean(), 
                            'wear_vanhees': timeseries['wear_vanhees'].groupby(timeseries.index // sampling_freq).min()}) 
-    enmo_1s.index = np.datetime64(start_datetime) + np.arange(len(enmo_1s.index))
+    enmo_1s.index = np.datetime64(start_datetime, "s") + np.arange(len(enmo_1s.index))
 
     # Final dataframes at 10s and 60s epochs (by averaging ENMO)
     enmo_10s = enmo_1s.resample('10s').mean()
